b0bb39044a5bb18d1597971ec0619a4618cc4a08
MatheusViniciusDosSantos <matheus.vinicius77@hotmail.com>
2023-06-14 22:35:37 -0300

    Criados testes dos valores limite superiores e inferiores para o resultado da submissão.

--- back/src/test/java/br/com/trabif/service/ResultadoSubmissaoTests.java
+++ back/src/test/java/br/com/trabif/service/ResultadoSubmissaoTests.java
@@ -1,6 +1,7 @@
 package br.com.trabif.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -28,13 +29,56 @@ class ResultadoSubmissaoTests {
         resultadoSubmissao.setComentarioOrganizador("Nada a comentar o autor foi pontual em suas afirmações");
         resultadoSubmissao.setConfianca(5);
         resultadoSubmissao.setResultado(5);
-        
 
         try {
             Mockito.when(resultadoSubmissaoService.save(resultadoSubmissao)).thenReturn(resultadoSubmissao);
             ResultadoSubmissao resultado = resultadoSubmissaoService.save(resultadoSubmissao);
     
             assertEquals(resultadoSubmissao, resultado);
+        } catch (BadResourceException | ResourceAlreadyExistsException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    void saveGradeLessThanValid() {
+        ResultadoSubmissao resultadoSubmissao = new ResultadoSubmissao();
+        resultadoSubmissao.setComentarioAutor("Trabalho ruim");
+        resultadoSubmissao.setComentarioOrganizador("Nada a comentar");
+        resultadoSubmissao.setConfianca(5);
+        resultadoSubmissao.setResultado(0);
+
+        try {
+            
+            Mockito.when(resultadoSubmissaoService.save(resultadoSubmissao)).thenThrow(
+                new IllegalArgumentException("O valor do resultado é menor que 1")
+            );
+            assertThrows(IllegalArgumentException.class, () -> {resultadoSubmissaoService.save(resultadoSubmissao);});
+    
+        } catch (BadResourceException | ResourceAlreadyExistsException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    void saveGradeGreaterThanValid() {
+        ResultadoSubmissao resultadoSubmissao = new ResultadoSubmissao();
+        resultadoSubmissao.setComentarioAutor("Trabalho muito bom");
+        resultadoSubmissao.setComentarioOrganizador("Nada a comentar o autor foi pontual em suas afirmações");
+        resultadoSubmissao.setConfianca(5);
+        resultadoSubmissao.setResultado(6);
+
+        try {
+            
+            Mockito.when(resultadoSubmissaoService.save(resultadoSubmissao)).thenThrow(
+                new IllegalArgumentException("O valor do resultado é maior que 5")
+            );
+            assertThrows(IllegalArgumentException.class, () -> {resultadoSubmissaoService.save(resultadoSubmissao);});
+    
         } catch (BadResourceException | ResourceAlreadyExistsException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
