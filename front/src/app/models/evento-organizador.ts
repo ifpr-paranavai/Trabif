07637f8a030f308a18d5deea7f09c8957af1e800
MatheusViniciusDosSantos <matheus.vinicius77@hotmail.com>
2023-08-30 10:49:55 -0300

    Remivido evento organizador

--- front/src/app/models/evento-organizador.ts
+++
@@ -1,9 +0,0 @@
-import { Usuario } from './usuario';
-import { Evento } from './evento';
-import { Auditoria } from "./auditoria";
-
-export class EventoOrganizador extends Auditoria {
-  id?: number;
-  eventoDTO?: Evento;
-  usuarioDTO?: Usuario;
-}
